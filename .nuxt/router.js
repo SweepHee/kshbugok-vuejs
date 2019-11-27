0b038226e0b5eb419bb137f840b5a8959e9cbede
Sweep <jeonsh1991@gmail.com>
2019-11-28 01:02:33 +0900

    header clear

--- .nuxt/router.js
+++ .nuxt/router.js
@@ -3,6 +3,7 @@ import Router from 'vue-router'
 import { interopDefault } from './utils'
 import scrollBehavior from './router.scrollBehavior.js'
 
+const _15723ada = () => interopDefault(import('..\\pages\\sub1.vue' /* webpackChunkName: "pages_sub1" */))
 const _8509c48e = () => interopDefault(import('..\\pages\\index.vue' /* webpackChunkName: "pages_index" */))
 
 Vue.use(Router)
@@ -15,6 +16,10 @@ export const routerOptions = {
   scrollBehavior,
 
   routes: [{
+    path: "/sub1",
+    component: _15723ada,
+    name: "sub1"
+  }, {
     path: "/",
     component: _8509c48e,
     name: "index"
--- layouts/default.vue
+++ layouts/default.vue
@@ -8,11 +8,11 @@
                 </div>
                 <nav>
                     <ul @mouseenter="hoverMenu = true">
-                        <li><nuxt-link to='/'>호텔소개</nuxt-link></li>
-                        <li><nuxt-link to='/'>객실안내</nuxt-link></li>
-                        <li><nuxt-link to='/'>시설안내</nuxt-link></li>
-                        <li><nuxt-link to='/'>커뮤니티</nuxt-link></li>
-                        <li><nuxt-link to='/'>예약하기</nuxt-link></li>
+                        <li @mouseenter="hoverNav(1)"><nuxt-link to='/'>호텔소개</nuxt-link><div v-if="turn1" class="hover-line"></div></li>
+                        <li @mouseenter="hoverNav(2)"><nuxt-link to='/sub1'>객실안내</nuxt-link><div v-if="turn2" class="hover-line"></div></li>
+                        <li @mouseenter="hoverNav(3)"><nuxt-link to='/'>시설안내</nuxt-link><div v-if="turn3" class="hover-line"></div></li>
+                        <li @mouseenter="hoverNav(4)"><nuxt-link to='/'>커뮤니티</nuxt-link><div v-if="turn4" class="hover-line"></div></li>
+                        <li @mouseenter="hoverNav(5)"><nuxt-link to='/'>예약하기</nuxt-link><div v-if="turn5" class="hover-line"></div></li>
                     </ul>
                 </nav>
                 <div>
@@ -27,9 +27,35 @@
                 </div>
             </div>
         </header>
-        <div v-if="hoverMenu" @mouseenter="hoverMenu = true" @mouseleave="hoverMenu = false" class="sub-menu">
-            
+        <div v-if="hoverMenu" @mouseenter="hoverMenu = true" @mouseleave="closeSubMenu" class="sub-menu">
+            <ul @mouseenter="hoverNav(1)">
+                <li><nuxt-link to="/">호텔소개</nuxt-link></li>
+                <li><nuxt-link to="/">이용안내</nuxt-link></li>
+                <li><nuxt-link to="/">오시는길</nuxt-link></li>
+                <li><nuxt-link to="/">주변관광지</nuxt-link></li>
+            </ul>
+            <ul @mouseenter="hoverNav(2)">
+                <li><nuxt-link to="/">레이싱룸</nuxt-link></li>
+                <li><nuxt-link to="/">캠핑룸</nuxt-link></li>
+                <li><nuxt-link to="/">글램핑룸</nuxt-link></li>
+                <li><nuxt-link to="/">핑크룸</nuxt-link></li>
+                <li><nuxt-link to="/">플레이룸</nuxt-link></li>
+                <li><nuxt-link to="/">캐리비안룸</nuxt-link></li>
+                <li><nuxt-link to="/">루프탑글램핑룸</nuxt-link></li>
+            </ul>
+            <ul @mouseenter="hoverNav(3)">
+                <li><nuxt-link to="/">키즈카페</nuxt-link></li>
+                <li><nuxt-link to="/">루프탑 물놀이터</nuxt-link></li>
+            </ul>
+            <ul @mouseenter="hoverNav(4)">
+                <li><nuxt-link to="/">공지사항</nuxt-link></li>
+                <li><nuxt-link to="/">이벤트</nuxt-link></li>
+            </ul>
+            <ul @mouseenter="hoverNav(5)">
+                <li><nuxt-link to="/">예약하기</nuxt-link></li>
+            </ul>
         </div>
+        <nuxt />
     </div>
 </template>
 
@@ -39,12 +65,38 @@ export default {
         return {
             hoverMenu: false,
             turnOn: false,
+            hoverActive: false,
+            turn1:false,
+            turn2:false,
+            turn3:false,
+            turn4:false,
+            turn5:false,
         }
     },
     methods: {
-        openSubmenu() {
-            this.hoverMenu = !this.hoverMenu;
-            this.turnOn = !this.turnOn;
+        closeSubMenu() {
+            this.hoverMenu = false;
+            this.turn1 = false;
+            this.turn2 = false;
+            this.turn3 = false;
+            this.turn4 = false;
+            this.turn5 = false;
+        },
+        hoverNav(i) {
+            this.turn1 = false;
+            this.turn2 = false;
+            this.turn3 = false;
+            this.turn4 = false;
+            this.turn5 = false;
+            switch(i)
+            {
+                case 1: this.turn1=true; break;
+                case 2: this.turn2=true; break;
+                case 3: this.turn3=true; break;
+                case 4: this.turn4=true; break;
+                case 5: this.turn5=true; break;
+            }
+
         }
     }
 }
@@ -57,13 +109,14 @@ export default {
 
     body {
         background: url("../img/bg/bg.png");
-        background-size: cover;
+        background-size: cover;        
+        font-family: 'Noto Sans KR', sans-serif;
+
     }
 
     header {
         width: 100%;
         height: 90px;
-        font-family: 'Noto Sans KR', sans-serif;
         background-color: #ffffff;
     }
 
@@ -85,6 +138,7 @@ export default {
     nav ul li {
         width: 140px;
         display: inline-block;
+        text-align: center;
     }
 
     nav ul li a {
@@ -97,18 +151,46 @@ export default {
         color: #a67c52;
     }
 
-    nav ul li a:hover:before {
-        content: "";
-        border-bottom: 2px solid #a67c52;
-        width: 65px;
-        top: 90px;
-        position: absolute;
-    }
-
     .sub-menu {
         width: 100%;
         height: 250px;
         background-color: #ffffff;
+        display: flex;
+        position: absolute;
+        justify-content: center;
+        padding-top: 20px;
+        padding-left:80px;
+    }
+
+    .sub-menu ul {
+        display: inline-block;
+        width: 140px;
+    }
+    .sub-menu ul:nth-child(3) {
+        padding-left:10px;
+    }
+
+    .sub-menu ul:nth-child(4) {
+        padding-left:15px;
+    }
+
+    .sub-menu ul:nth-child(5) {
+        padding-left:15px;
+    }
+
+    .sub-menu ul li {
+        height: 30px;
+        font-size: 15px;
+        display: block;
+        text-align: center;
+    }
+
+    .sub-menu ul li a {
+        color:#363636;
+    }
+
+    .sub-menu ul li a:hover {
+        color:#a67c52;
     }
 
 
