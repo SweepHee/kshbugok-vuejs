e5eceb047804b327681ea7a1d277ada25447af90
Sweep <jeonsh1991@gmail.com>
2019-12-18 00:53:35 +0900

    copy

--- server.js
+++ server.js
@@ -1,27 +1,27 @@
-const { Nuxt, Builder } = require('nuxt')
+const { Nuxt, Builder } = require('nuxt');
 
-const app = require('express')()
-const isProd = (process.env.NODE_ENV === 'production')
-const port = process.env.PORT || 3000
+const app = require('express')();
+const isProd = (process.env.NODE_ENV === 'production');
+const port = process.env.PORT || 3081;
 
 // We instantiate Nuxt.js with the options
-const config = require('./nuxt.config.js')
-config.dev = !isProd
-const nuxt = new Nuxt(config)
+const config = require('./nuxt.config.js');
+config.dev = !isProd;
+const nuxt = new Nuxt(config);
 
 // Render every route with Nuxt.js
-app.use(nuxt.render)
+app.use(nuxt.render);
 
 // Build only in dev mode with hot-reloading
 if (config.dev) {
     new Builder(nuxt).build()
-        .then(listen)
+        .then(listen);
 } else {
-    listen()
+    listen();
 }
 
-function listen () {
+function listen() {
     // Listen the server
-    app.listen(port, '0.0.0.0')
-    console.log('Server listening on `localhost:' + port + '`.')
+    app.listen(port);
+    console.log('Server listening on `localhost:' + port + '`.');
 }
