37388201a24c233421b7dfe04da8b82811adb38c
Sweep <jeonsh1991@gmail.com>
2019-12-17 23:15:56 +0900

    moment

--- .nuxt/index.js
+++ .nuxt/index.js
@@ -13,6 +13,7 @@ import { setContext, getLocation, getRouteData, normalizeError } from './utils'
 
 import nuxt_plugin_plugin_45f48794 from 'nuxt_plugin_plugin_45f48794' // Source: .\\vuetify\\plugin.js (mode: 'all')
 import nuxt_plugin_axios_7eb47cbe from 'nuxt_plugin_axios_7eb47cbe' // Source: .\\axios.js (mode: 'all')
+import nuxt_plugin_moment_62be0417 from 'nuxt_plugin_moment_62be0417' // Source: .\\moment.js (mode: 'all')
 
 // Component: <ClientOnly>
 Vue.component(ClientOnly.name, ClientOnly)
@@ -152,6 +153,10 @@ async function createApp (ssrContext) {
 
   if (typeof nuxt_plugin_axios_7eb47cbe === 'function') {
     await nuxt_plugin_axios_7eb47cbe(app.context, inject)
+  }
+
+  if (typeof nuxt_plugin_moment_62be0417 === 'function') {
+    await nuxt_plugin_moment_62be0417(app.context, inject)
   }
 
   // If server-side, wait for async component to be resolved first
--- nuxt.config.js
+++ nuxt.config.js
@@ -7,9 +7,26 @@ module.exports = {
     ],
     buildModules: [
         "@nuxtjs/vuetify",
+        '@nuxtjs/moment',
     ],
+    moment: {
+        locales: ['ko'],
+    },
     plugins: [],
     vuetify: {
 
+    },
+    build: {
+        analyze: true,
+        extend(config, { isClient, isServer, isDev }) {
+            console.log("webpack", config, isServer, isClient);
+
+            if(isServer && !isDev) {
+                config.devtool = 'hidden-source-map';
+            }
+        },
+    },
+    server: {
+        port: process.env.PORT || 3081,
     }
 };
